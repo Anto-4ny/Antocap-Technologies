6ef05735ca5f6704407c34dc4b2899086c3578e5
Anto-4ny <143907986+Anto-4ny@users.noreply.github.com>
2024-05-27 07:34:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", () => {
         showSlide(currentSlide);
     };
 
-    setInterval(nextSlide, 3000); // Change slide every 3 seconds
+    setInterval(nextSlide, 6000); // Change slide every 3 seconds
 
     // Hamburger menu functionality
     const menuIcon = document.getElementById('menu-icon');
