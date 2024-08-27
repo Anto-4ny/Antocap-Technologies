b79156f536a5823c6b431ae19f5b94e45d01ef79
Antony Ndambuki <143907986+Anto-4ny@users.noreply.github.com>
2024-08-27 09:13:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -220,4 +220,22 @@ function redirectToContactUs() {
     window.location.href = "contact-us.html";
     }
         
-              
+              //pop ins
+document.addEventListener('DOMContentLoaded', () => {
+    const elements = document.querySelectorAll('.pop-in');
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('active');
+            }
+        });
+    }, {
+        threshold: 0.1
+    });
+
+    elements.forEach(element => {
+        observer.observe(element);
+    });
+});
+                    
